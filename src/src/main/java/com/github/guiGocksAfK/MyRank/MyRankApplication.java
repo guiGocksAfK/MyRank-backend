1dd2a23e972d4146d2a4cd5ca78ed52b1e0d1634
guigocks-ux <guigocks@gmail.com>
2026-05-04 17:49:02 -0300

    Adicionado springbot

---
+++ src/src/main/java/com/github/guiGocksAfK/MyRank/MyRankApplication.java
@@ -0,0 +1,13 @@
+package com.github.guiGocksAfK.MyRank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRankApplication.class, args);
+	}
+
+}
---
+++ src/src/test/java/com/github/guiGocksAfK/MyRank/MyRankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.guiGocksAfK.MyRank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
