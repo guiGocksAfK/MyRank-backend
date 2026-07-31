2558c627a280fae6e3195841f6665fa1372af25a
guigocks-ux <guigocks@gmail.com>
2026-07-31 20:10:39 -0300

    criacao do "authUtils" e ajustes no UserController e UserService que agora passam a utilizalo

--- src/main/java/br/com/myrank/controller/UserController.java
+++ src/main/java/br/com/myrank/controller/UserController.java
@@ -4,10 +4,11 @@ import br.com.myrank.domain.entity.User;
 import br.com.myrank.dto.UserCreateDTO;
 import br.com.myrank.dto.UserResponseDTO;
 import br.com.myrank.dto.UserUpdateDTO;
+import br.com.myrank.security.AuthUtils;
 import br.com.myrank.service.UserService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,15 +16,17 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final AuthUtils authUtils;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthUtils authUtils) {
         this.userService = userService;
+        this.authUtils = authUtils;
     }
 
     @PostMapping
     public ResponseEntity<UserResponseDTO> create(@RequestBody UserCreateDTO dto) {
         User user = userService.createUser(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(UserResponseDTO.fromEntity(user));
+        return ResponseEntity.ok(UserResponseDTO.fromEntity(user));
     }
 
     @GetMapping("/{id}")
@@ -33,20 +36,17 @@ public class UserController {
     }
 
     @GetMapping("/me")
-    public ResponseEntity<UserResponseDTO> getCurrentUser(Authentication authentication) {
-        String email = authentication.getName();
-        User user = userService.getByEmail(email);
+    public ResponseEntity<UserResponseDTO> getMe(@AuthenticationPrincipal UserDetails userDetails) {
+        User user = authUtils.getUser(userDetails);
         return ResponseEntity.ok(UserResponseDTO.fromEntity(user));
     }
 
     @PutMapping("/me")
-    public ResponseEntity<UserResponseDTO> updateCurrentUser(
-            Authentication authentication,
-            @RequestBody UserUpdateDTO dto
-    ) {
-        String email = authentication.getName();
-        User currentUser = userService.getByEmail(email);
-        User updated = userService.updateUser(currentUser.getId(), dto);
+    public ResponseEntity<UserResponseDTO> updateMe(
+            @AuthenticationPrincipal UserDetails userDetails,
+            @RequestBody UserUpdateDTO dto) {
+        User user = authUtils.getUser(userDetails);
+        User updated = userService.updateUser(user.getId(), dto);
         return ResponseEntity.ok(UserResponseDTO.fromEntity(updated));
     }
 }
