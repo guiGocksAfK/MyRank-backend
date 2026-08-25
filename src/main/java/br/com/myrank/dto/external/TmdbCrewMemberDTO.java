7725592ff8dc12a22807174543ea0bdf358ceba9
guigocks-ux <guigocks@gmail.com>
2026-08-25 18:21:48 -0300

    Conexao com a API da Tmdb pt2

---
+++ src/main/java/br/com/myrank/dto/external/TmdbCrewMemberDTO.java
@@ -0,0 +1,16 @@
+package br.com.myrank.dto.external;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TmdbCrewMemberDTO {
+
+    private String name;
+    private String job;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getJob() { return job; }
+    public void setJob(String job) { this.job = job; }
+}
