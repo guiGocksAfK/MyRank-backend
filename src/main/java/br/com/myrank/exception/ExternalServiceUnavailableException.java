604cff2291183bb9502377e7ad1e1abc11d4e66c
guigocks-ux <guigocks@gmail.com>
2026-08-27 16:48:23 -0300

    Tratamento de erros pt1

---
+++ src/main/java/br/com/myrank/exception/ExternalServiceUnavailableException.java
@@ -0,0 +1,13 @@
+package br.com.myrank.exception;
+
+/**
+ * Lançada quando uma API externa (TMDB, RAWG, Jikan, etc.) está indisponível
+ * ou retorna erro de servidor (5xx). Sinaliza ao controller que o problema é
+ * externo, não uma falha de autenticação ou dado inválido do usuário.
+ */
+public class ExternalServiceUnavailableException extends RuntimeException {
+
+    public ExternalServiceUnavailableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
